0d38a59a7f9ec76d0b1de81759a6b1c34458ad87
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-06-28 00:51:23 +0500

    Learnt basics of arrow function

---
+++ arrow-functions-basics.js
@@ -0,0 +1,30 @@
+// Arrow Function
+
+// A simple and concise syntax for writing function expressions
+
+// let func = (arg1, arg2, arg3) => expression ;
+/*
+let sum = (a, b) => a + b;
+
+alert(sum(2 , 3))
+
+*/
+/*
+let double = n => n * 2;
+
+alert(double(3))
+*/
+
+// Task 
+/*
+let ask = (question, yes, no) => {
+    if(confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do You agree?",
+    () => alert('You agreed'),
+    () => alert('You canceled the execution.')
+)
+*/
